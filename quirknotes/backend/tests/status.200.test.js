eddc761a3bbb2b7c9bddbf266b91535988d215a5
hiromikoreyes <[200~114784445+hiromikoreyes@users.noreply.github.com>
2024-02-14 18:08:24 -0500

    setup dev branch

--- quirknotes/backend/tests/status.200.test.js
+++ quirknotes/backend/tests/status.200.test.js
@@ -29,46 +29,15 @@ test("/postNote - Post a note", async () => {
 });
 
 test("/getAllNotes - Return list of zero notes for getAllNotes", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/getAllNotes - Return list of two notes for getAllNotes", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/deleteNote - Delete a note", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/patchNote - Patch with content and title", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/patchNote - Patch with just title", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/patchNote - Patch with just content", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/deleteAllNotes - Delete one note", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/deleteAllNotes - Delete three notes", async () => {
-    // Code here
-    expect(false).toBe(true);
-  });
-  
-  test("/updateNoteColor - Update color of a note to red (#FF0000)", async () => {
-    // Code here
-    expect(false).toBe(true);
+    const getAllNotesRes = await fetch(`${SERVER_URL}/getAllNotes`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: title,
+          content: content,
+        }),
+      });
+      
   });
